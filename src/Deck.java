b7bdee98bda6e60ed953241d6b941c3d7baa092f
lkassaei <lily.kassaei@menloschool.org>
2024-11-18 09:58:34 -0800

    Started Deck and finished Player

---
+++ src/Deck.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> cards;
+    private int cardsLeft;
+
+    public Deck(String[] ranks, String[] suits, int[] values) {
+
+    }
+}
